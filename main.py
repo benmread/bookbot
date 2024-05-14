f47a9d505b02ea03d5983cd96f20d3f00954d81a
benmread <benmread@gmail.com>
2024-05-14 10:18:02 +0100

    Sorted list function completed

--- main.py
+++ main.py
@@ -5,22 +5,38 @@ def main():
         file_contents = f.read()
 
     print(len(wordcount(file_contents)))
-    charactercount(file_contents)
+    sortedlist(charactercount(file_contents))
+
 
 def wordcount(text):
     return text.split()
 
+
 def charactercount(text):
     char_counter = {}
+    char_list = []
     text = text.lower()
 
     for letter in string.printable: # all ASCII characters
         if letter in text and letter not in char_counter:
             char_counter[letter] = text.count(letter)
 
-    char_list = char_counter.items()
-    char_list.sort(reverse=True)
-    print(char_list)
+    return char_counter
+
+
+def sortedlist(char_dict):
+    sorted_list = []
+    for key in char_dict:
+        sorted_list.append({"char": key, "num": char_dict[key]})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["num"]
 
 
 main()
